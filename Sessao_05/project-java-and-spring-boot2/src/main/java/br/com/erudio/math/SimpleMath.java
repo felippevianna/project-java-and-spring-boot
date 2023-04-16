63daaf2f7387db9bc7579670db697f5519aceeb5
felippevianna <felippe.conduru@gmail.com>
2023-04-16 11:56:49 -0300

    Finalizando secao 6 do curso

---
+++ Sessao_05/project-java-and-spring-boot2/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,32 @@
+package br.com.erudio.math;
+
+
+public class SimpleMath {
+	
+	public Double sum(Double numberOne, Double numberTwo){
+		return numberOne + numberTwo;
+	}
+	
+	public Double sub(Double numberOne, Double numberTwo){
+		return numberOne - numberTwo;
+	}
+	
+	
+	public Double multi(Double numberOne, Double numberTwo){
+		return numberOne * numberTwo;
+	}
+	
+	public Double div(Double numberOne, Double numberTwo) {
+		return numberOne / numberTwo;
+	}
+	
+	
+	public Double media(Double numberOne, Double numberTwo){
+		return (numberOne +numberTwo) / 2;
+	}
+	
+	public Double squareRoot(Double numberOne) {
+		return Math.sqrt(numberOne);
+	}
+		
+}
